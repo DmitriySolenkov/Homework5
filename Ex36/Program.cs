4f248076a683f887393da4cda67c4635c18e8f8b
KreoX <soldimksol@gmail.com>
2022-08-21 15:12:04 +0300

    Ex36

---
+++ Ex36/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] RandomArray(int leng)
+{
+    int[] arr = new int[leng];
+    for (int i = 0; i < leng; i++)
+    {
+        arr[i] = new Random().Next(0, 99);
+    }
+    return arr;
+}
+
+
+Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = RandomArray(length);
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int sum=0;
+for (int j = 0; j < length; j++)
+{
+    if (j%2!=0)
+    {
+        sum += array[j];
+    }
+}
+
+Console.WriteLine($"Сумма элементов массива, стояших на нечетных позициях: {sum}");
