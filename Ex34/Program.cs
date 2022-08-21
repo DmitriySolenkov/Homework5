87bfb1728ffa31028551ff9938873888c7ae6dc4
KreoX <soldimksol@gmail.com>
2022-08-21 15:05:56 +0300

    Ex34

---
+++ Ex34/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] RandomArray(int leng)
+{
+    int[] arr = new int[leng];
+    for (int i = 0; i < leng; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+
+Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = RandomArray(length);
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int count=0;
+for (int j = 0; j < length; j++)
+{
+    if (array[j]%2==0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Четных чисел в массиве: {count}");
